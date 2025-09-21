8add4b6c9a30b0aa7a01a16d8be788bd1ea54c70
124c4a11 <124c4a11@mail.ru>
2025-09-21 19:37:03 +0300

    399. Evaluate Division (medium)

---
+++ 11. Graphs/151-250/7. 399. Evaluate Division (medium)/399. Evaluate Division (solution).js
@@ -0,0 +1,44 @@
+/**
+ * @param {string[][]} equations
+ * @param {number[]} values
+ * @param {string[][]} queries
+ * @return {number[]}
+ */
+function calcEquation(equations, values, queries) {
+  const adj = new Map();
+  for (let i = 0; i < equations.length; i++) {
+    const [a, b] = equations[i];
+
+    if (!adj.has(a)) adj.set(a, []);
+    if (!adj.has(b)) adj.set(b, []);
+
+    adj.get(a).push([b, values[i]]);
+    adj.get(b).push([a, 1 / values[i]]);
+  }
+
+  function dfs(src, target, visited) {
+    if (
+      !adj.has(src)
+      || !adj.has(target)
+    ) return -1;
+    if (src === target) return 1;
+
+    visited.add(src);
+
+    for (const [nei, weight] of adj.get(src)) {
+      if (visited.has(nei)) continue;
+
+      const result = dfs(nei, target, visited);
+
+      if (result !== -1) {
+        return weight * result;
+      }
+    }
+
+    return -1;
+  };
+
+  return queries.map(
+    ([src, target]) => dfs(src, target, new Set())
+  );
+}
