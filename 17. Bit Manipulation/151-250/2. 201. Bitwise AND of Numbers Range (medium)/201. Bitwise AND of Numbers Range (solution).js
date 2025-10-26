840b728fc853c7c021fa0c22cc55c49c65b0ba6b
124c4a11 <124c4a11@mail.ru>
2025-10-26 16:22:50 +0300

    201. Bitwise AND of Numbers Range (medium)

---
+++ 17. Bit Manipulation/151-250/2. 201. Bitwise AND of Numbers Range (medium)/201. Bitwise AND of Numbers Range (solution).js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+function rangeBitwiseAnd(left, right) {
+  while (left < right) {
+    right &= right - 1;
+  }
+
+  return right;
+}
