6f18345348a434eb18709ddce875cf7217d0ab03
124c4a11 <124c4a11@mail.ru>
2025-10-30 09:40:51 +0300

    724. Find Pivot Index (easy)

---
+++ 1. Arrays & Hashing/251+/9. 724. Find Pivot Index (easy)/724. Find Pivot Index (solution).js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function pivotIndex(nums) {
+  const total = nums.reduce((a, b) => a + b, 0);
+
+  let leftSum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    const rightSum = total - leftSum - nums[i];
+
+    if (leftSum === rightSum) return i;
+
+    leftSum += nums[i];
+  }
+
+  return -1;
+}
