cfa4e1bf7ca8dbc88241207c424915d85d98c8c1
124c4a11 <124c4a11@mail.ru>
2025-11-02 08:57:13 +0300

    448. Find All Numbers Disappeared in an Array (easy)

---
+++ 1. Arrays & Hashing/251+/12. 448. Find All Numbers Disappeared in an Array (easy)/448. Find All Numbers Disappeared in an Array (solution).js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+function findDisappearedNumbers(nums) {
+  for (const num of nums) {
+    const i = Math.abs(num) - 1;
+    nums[i] = -Math.abs(nums[i]);
+  }
+
+  const result = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) result.push(i + 1);
+  }
+
+  return result;
+}
