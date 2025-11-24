39608dae32995a66dd6216d3c00588544c0bdf1f
124c4a11 <124c4a11@mail.ru>
2025-11-24 11:52:40 +0300

    1897. Redistribute Characters to Make All Strings Equal (easy)

---
+++ 1. Arrays & Hashing/251+/34. 1897. Redistribute Characters to Make All Strings Equal (easy)/1897. Redistribute Characters to Make All Strings Equal (solution).js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} words
+ * @return {boolean}
+ */
+function makeEqual(words) {
+  const charFreq = new Int32Array(26);
+  for (const word of words) {
+    for (const char of word) {
+      charFreq[char.charCodeAt(0) - 97]++;
+    }
+  }
+
+  for (const freq of charFreq) {
+    if (freq % words.length !== 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
