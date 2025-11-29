ef4515fd418d49f6f52d4d9edbe0720a5e38dc3c
124c4a11 <124c4a11@mail.ru>
2025-11-29 13:36:56 +0300

    1165. Single-Row Keyboard (easy)

---
+++ 1. Arrays & Hashing/251+/44. 1165. Single-Row Keyboard (easy)/1165. Single-Row Keyboard (solution).js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} keyboard
+ * @param {string} word
+ * @return {number}
+ */
+function calculateTime(keyboard, word) {
+  const charToNdx = new Int32Array(26);
+  for (let i = 0; i < keyboard.length; i++) {
+    charToNdx[keyboard[i].charCodeAt(0) - 97] = i;
+  }
+
+  let result = 0;
+  let prevNdx = 0;
+  for (const char of word) {
+    const charNdx = charToNdx[char.charCodeAt(0) - 97];
+
+    result += Math.abs(charNdx - prevNdx);
+    prevNdx = charNdx;
+  }
+
+  return result;
+}
