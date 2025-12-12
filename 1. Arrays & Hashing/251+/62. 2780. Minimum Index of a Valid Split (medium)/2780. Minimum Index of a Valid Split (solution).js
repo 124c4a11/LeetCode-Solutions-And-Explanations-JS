a6058ae1871e7866df6d7d309dc806f0f505b36a
124c4a11 <124c4a11@mail.ru>
2025-12-12 15:50:53 +0300

    2780. Minimum Index of a Valid Split (medium)

---
+++ 1. Arrays & Hashing/251+/62. 2780. Minimum Index of a Valid Split (medium)/2780. Minimum Index of a Valid Split (solution).js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function minimumIndex(nums) {
+  const n = nums.length;
+
+  let majority = 0;
+  let cnt = 0;
+  for (const num of nums) {
+    if (cnt === 0) majority = num;
+    cnt += num === majority ? 1 : -1;
+  }
+
+  let leftMajorityCnt = 0;
+  let rightMajorityCnt = nums.filter(
+    (x) => x === majority
+  ).length;
+  for (let i = 0; i < n; i++) {
+    if (nums[i] === majority) {
+      leftMajorityCnt++;
+      rightMajorityCnt--;
+    }
+
+    const leftLen = i + 1;
+    const rightLen = n - i - 1;
+
+    if (
+      leftMajorityCnt * 2 > leftLen
+      && rightMajorityCnt * 2 > rightLen
+    ) return i;
+  }
+
+  return -1;
+}
