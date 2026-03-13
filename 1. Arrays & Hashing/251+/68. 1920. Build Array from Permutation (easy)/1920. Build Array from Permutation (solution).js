885892f305bcf1f6964a6db5eb6eba2875a7a4ab
124c4a11 <124c4a11@mail.ru>
2026-03-13 15:37:50 +0300

    1920. Build Array from Permutation (easy)

---
+++ 1. Arrays & Hashing/251+/68. 1920. Build Array from Permutation (easy)/1920. Build Array from Permutation (solution).js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+function buildArray(nums) {
+  const n = nums.length;
+
+  for (let i = 0; i < n; i++) {
+    nums[i] += n * (nums[nums[i]] % n);
+  }
+
+  for (let i = 0; i < n; i++) {
+    nums[i] = Math.floor(nums[i] / n);
+  }
+
+  return nums;
+}
