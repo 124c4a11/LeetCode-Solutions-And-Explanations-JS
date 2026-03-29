195f91efe0a2b356e7dcae06c2bfd9d7c7326b83
124c4a11 <124c4a11@mail.ru>
2026-03-29 15:20:33 +0300

    1389. Create Target Array in the Given Order (easy)

---
+++ 1. Arrays & Hashing/251+/82. 1389. Create Target Array in the Given Order (easy)/1389. Create Target Array in the Given Order (solution).js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number[]} index
+ * @return {number[]}
+ */
+function createTargetArray(nums, index) {
+  const result = [];
+  for (let i = 0; i < nums.length; i++) {
+    result.splice(index[i], 0, nums[i]);
+  }
+
+  return result;
+}
