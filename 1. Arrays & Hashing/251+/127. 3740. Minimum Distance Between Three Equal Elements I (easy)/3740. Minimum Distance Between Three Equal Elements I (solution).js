b5af6e8d3c239f1245bcccf58bfe2aee80a31bce
124c4a11 <124c4a11@mail.ru>
2026-06-15 13:22:04 +0300

    3740. Minimum Distance Between Three Equal Elements I (easy)

---
+++ 1. Arrays & Hashing/251+/127. 3740. Minimum Distance Between Three Equal Elements I (easy)/3740. Minimum Distance Between Three Equal Elements I (solution).js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function minimumDistance(nums) {
+  const numToSeenIndices = new Array(101);
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+
+    if (!numToSeenIndices[num]) {
+      numToSeenIndices[num] = [];
+    }
+
+    numToSeenIndices[num].push(i);
+  }
+
+  let minDistance = Infinity;
+  for (let num = 1; num < 101; num++) {
+    const indices = numToSeenIndices[num];
+    if (
+      !indices
+      || indices.length < 3
+    ) continue;
+
+    for (let i = 0; i < indices.length - 2; i++) {
+      const a = indices[i];
+      const b = indices[i + 1];
+      const c = indices[i + 2];
+
+      const currDistance =
+        Math.abs(a - b)
+        + Math.abs(b - c)
+        + Math.abs(c - a);
+
+      minDistance = Math.min(minDistance, currDistance);
+    }
+  }
+
+  return minDistance === Infinity ? -1 : minDistance;
+}
