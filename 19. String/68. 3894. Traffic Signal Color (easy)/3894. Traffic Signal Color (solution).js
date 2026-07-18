fcb75b0e2b1e64fb306fe26415a442b9995d5d36
124c4a11 <124c4a11@mail.ru>
2026-07-18 17:21:03 +0300

    3894. Traffic Signal Color (easy)

---
+++ 19. String/68. 3894. Traffic Signal Color (easy)/3894. Traffic Signal Color (solution).js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} timer
+ * @return {string}
+ */
+function trafficSignal(timer) {
+  if (timer === 0) return 'Green';
+  if (timer === 30) return 'Orange';
+  if (timer > 30 && timer <= 90) return 'Red';
+
+  return 'Invalid';
+}
