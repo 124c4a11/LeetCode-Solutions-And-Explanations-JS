8729091e5acc478ddea90a1a7505547a3a932128
124c4a11 <124c4a11@mail.ru>
2026-07-27 18:03:57 +0300

    412. Fizz Buzz (easy)

---
+++ 19. String/76. 412. Fizz Buzz (easy)/412. Fizz Buzz (solution).js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+function fizzBuzz(n) {
+  const result = new Array(n);
+  for (let i = 0; i < n; i++) {
+    const num = i + 1;
+
+    if (
+      num % 3 === 0
+      && num % 5 === 0
+    ) result[i] = 'FizzBuzz';
+    else if (num % 3 === 0) result[i] = 'Fizz';
+    else if (num % 5 === 0) result[i] = 'Buzz';
+    else result[i] = `${num}`;
+  }
+
+  return result;
+}
